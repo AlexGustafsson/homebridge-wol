6715b206e4061e8d27d203a4b41e43c099c78abf
Alex Gustafsson <alex-gustafsson@live.se>
2016-01-13 20:26:49 +0100

    Added ability to see if a computer is turned on or not. Heavily improved documentation.

--- README.md
+++ README.md
@@ -1,16 +1,53 @@
-# Wake on Lan Plugin
+# Wake on Lan Plugin for Homebridge
+### Turn your computer on through Siri
+***
 
-Example config.json:
+### Setting up
 
- ```
-    "accessories": [
-        {
-          "accessory": "Computer",
-          "name": "My Computer",
-          "mac": "<mac-address>"
-        }   
-    ]
+###### Installing
 
+To install the plugin, head over to the machine with Homebridge set up and run
+```
+npm install -g homebridge-wol
 ```
 
-With this plugin you can start your computers with HomeKit-enabled apps.
+###### Configuration
+
+To make Homebridge aware of the new plugin, you will have to add it to your configuration usually found in `/root/.homebridge/config.json`. Somewhere inside that file you should see a key named `accessories`. This is where you can add your computer as shown here:
+
+ ```json
+"accessories": [
+    {
+      "accessory": "Computer",
+      "name": "My Gaming Rig",
+      "mac": "<mac-address>",
+      "ip": "192.168.1.151"
+    },
+    {
+      "accessory": "Computer",
+      "name": "My Macbook",
+      "mac": "<mac-address>"
+    }
+]
+```
+
+###### Options
+
+| Key       | Description                                                     | Required |
+| --------- | --------------------------------------------------------------- | ---------|
+| accessory | The type of accessory - has to be "Computer"                    | Yes      |
+| name      | The name of the computer - used in HomeKit apps as well as Siri | Yes      |
+| mac       | The computer's MAC address - used to send Magic Packets         | Yes      |
+| ip        | The IPv4 address of the computer - used to check current status | No       |
+
+### Usage
+
+To use this package you need a HomeKit-enabled app. When you've gone through the setup there should be a switch showing in the app with the name of your computer. If the switch is off and you tap it, your computer will be turned on. However, you cannot turn off your computer with this plugin, so by switching the switch off - nothing will happen and it will soon go back to being on. Whenever the computer shuts down the switch will be automatically switched off.
+
+If you haven't yet found an applicable app, I recommend the following:
+
+##### [iDevices](https://itunes.apple.com/se/app/idevices-connected/id682656390?mt=8)
+iDevices is a great app to configure everything HomeKit related. It offers great control over houses, rooms, scenes and more.
+
+#### [Beam](https://itunes.apple.com/us/app/beam-elevate-your-home/id1038439712?mt=8)
+Beam is a stylish, minimalistic approach to a home remote. Whilst not offering the configurability of iDevices, Beam is targeting the everyday use with a great user experience.
--- index.js
+++ index.js
@@ -1,46 +1,142 @@
 "use strict";
 
+//Require the wake on lan utility package
 var wol = require("wake_on_lan");
+
+//Require the net ping utility package
+var ping = require("net-ping");
 
 var Service, Characteristic;
 
+//Export homebridge plugin
 module.exports = function(homebridge) {
 
+  //Determine the service - part of the homebridge plugin ecosystem
   Service = homebridge.hap.Service;
+
+  //Determine charachteristics (i.e. switch, lamp etc.) - part of the homebridge plugin ecosystem
   Characteristic = homebridge.hap.Characteristic;
 
+  //Register the plugin as an accessory "Computer" of type Computer - part of the homebridge plugin ecosystem
   homebridge.registerAccessory("homebridge-wol", "Computer", Computer);
 }
 
+//Computer class
 function Computer(log, config) {
+  //The log to which the plugin writes debug messages - part of homebridge plugin ecosystem
   this.log = log;
+
+  //The name of the computer, used for ease of access as well as Siri implementations
   this.name = config.name;
+
+  //The mac address of the computer, used to create a "magic package" for WoL
   this.mac = config.mac;
 
+  //The ip address of the computer, used to ping the machine to check if it's turned on or not. Currently only supports ipv4
+  this.ip = config.ip;
+
+  //Create a session which can be used to ping the device - part of the net-ping package
+  if(this.ip)
+    this._session = ping.createSession();
+
+  //Make the computer a "switch" which can be on or off - part of the homebridge plugin ecosystem
   this._service = new Service.Switch(this.name);
-  this._service.getCharacteristic(Characteristic.On)
-    .on('set', this._setOn.bind(this));
+  //Handle what happens when the switch is supposed to be turned on
+  this._service.getCharacteristic(Characteristic.On).on('set', this._setOn.bind(this));
 }
 
+//Function to retrieve the services that the plugin is capabel of - part of the homebridge plugin ecosystem
 Computer.prototype.getServices = function() {
   return [this._service];
 }
 
+//Function which handles what happens when the switch is supposed to be turned on
 Computer.prototype._setOn = function(on, callback) {
-
+  //If the new state is 'on'
   if(on){
+    //Send magic packages to the computer
     wol.wake(this.mac, function(error) {
+      //If an error occured when sending the packages
       if (error) {
+        //Turn the switch off
         this._service.setCharacteristic(Characteristic.On, false);
+        //Log error
         this.log("Error when sending packets", error);
       } else {
+        //Log that the packets were sent
         this.log("Packets sent");
+        //Wait 30 seconds to give the computer some time
         setTimeout(function() {
-          this._service.setCharacteristic(Characteristic.On, false);
-        }.bind(this), 30000);
+          //Define a "timer"
+          var stateTimer = null;
+
+          //Define how often the computer's status should be logged (the default, 5, means every 5th minute)
+          var logInterval = 5;
+          //The current number (step) of sequential checks
+          var currentStep = 1;
+          //Whether or not the status should be logged
+          var shouldLog = false;
+
+          //Start the timer which checks the computer every 30 seconds
+          stateTimer = setInterval(function() {
+            //Up currentStep by one
+            currentStepp++;
+
+            //Check if the step has surpassed the wanted log interval
+            if(currentStep >= logInterval){
+              //Tell the state checker to log
+              log = true;
+              //Reset the number of checks
+              currentStep = 1;
+            }
+
+            this.checkState(shouldLog, function(pingError) {
+              //Check if there was an error (the computer was unreachable)
+              if(pingError){
+                //Stop checking the computer's status by stopping the timer
+                clearInterval(stateTimer);
+
+                //Turn the switch off
+                this._service.setCharacteristic(Characteristic.On, false);
+              }
+            }.bind(this));
+          }).bind(this), 30 * 1000);
+        }.bind(this), 30 * 1000);
       }
     }.bind(this));
   }
 
+  //Tell homebridge that the plugin is done progressing the event
   callback();
+}
+
+//Function that checks the computer to see if it's turned on
+Computer.prototype.checkState = function(shouldLog, callback) {
+  //Ping the computer
+  this.session.pingHost(this.ip, function(error){
+    //If an error occured when checking the machine's current state
+    if(error){
+      //If the computer is not awake
+      if(error instanceof ping.RequestTimedOutError) {
+        //Tell the calling method that the device is unreachable
+        callback(error);
+
+        //Log that the computer is no longer active
+        this.log("The computer " + this.ip + " is no longer active");
+      } else {
+        //Tell the calling method that an error occured
+        callback(error);
+
+        //Log the error
+        this.log(JSON.stringify(error));
+      }
+    } else {
+      //Tell the calling method that the computer is turned on
+      callback(null);
+
+      //Check to see if the event should be logged
+      if(shouldLog)
+        this.log("The computer " + this.ip + " is active");
+    }
+  }.bind(this));
 }
