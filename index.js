c2d0104ce7000c67bddc153653a4ab3f1f782c6b
michael <michael@macbookpro.local>
2020-08-04 03:52:03 +0200

    small fixes

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-// const getNetworkDevice = require('./lib/network-device');
+const getNetworkDevice = require('./lib/network-device');
 
-// function setup(homebridge) {
-//   const NetworkDevice = getNetworkDevice(homebridge.hap);
-
-//   homebridge.registerAccessory('homebridge-wol', 'NetworkDevice', NetworkDevice);
-// }
-
-// module.exports = setup;
-
-module.exports = (homebridge) => {
-  Service = homebridge.hap.Service;
-  Characteristic = homebridge.hap.Characteristic;
-  const NetworkDevice = require('./lib/network-device');
-  FakeGatoHistoryService = require('fakegato-history')(homebridge);
-
+function setup(homebridge) {
+  const NetworkDevice = getNetworkDevice(homebridge);
   homebridge.registerAccessory('homebridge-wol', 'NetworkDevice', NetworkDevice);
-};
+}
+
+module.exports = setup;
